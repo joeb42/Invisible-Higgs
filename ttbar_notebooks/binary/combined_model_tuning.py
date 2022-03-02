af418c4c5cb58099808cbfe0345fd46574c68b2f
Joe Button [HF 2110] <ac18804@gpu01.dice.priv>
2022-03-02 11:12:34 +0000

    multiclass and CNN updates

--- ttbar_notebooks/binary/combined_model_tuning.py
+++ ttbar_notebooks/binary/combined_model_tuning.py
@@ -36,13 +36,13 @@ class HyperModel(kt.HyperModel):
         - Drop out
         """
         obj_input = Input(shape=(14, 8))
-        lstm = LSTM(hp.Int("RNN units", 100, 200, 10))(obj_input)
-        ln = LayerNormalization()(lstm)
+        gru = GRU(hp.Int("RNN units", 100, 200, 10))(obj_input)
+        ln = LayerNormalization()(gru)
         units = hp.Int("mlp neurons per layer", 50, 200, 25)
         dropout_rate = hp.Float("dropout rate", 0, 0.4, 0.05)
         n_layers = hp.Int("mlp layers", 1, 5, 1)
         combine_layer = hp.Int("combine position", 1, n_layers)
-        event_input = Input(shape=(11,))
+        event_input = Input(shape=(10,))
         prev = event_input
         for n in range(n_layers):
             layer = Dense(units, activation="selu", kernel_initializer="lecun_normal")(
@@ -51,7 +51,11 @@ class HyperModel(kt.HyperModel):
             if n == combine_layer - 1:
                 layer = Concatenate()([ln, layer])
             prev = AlphaDropout(rate=dropout_rate)(layer)
-        out = Dense(self.output_neurons, activation="sigmoid")(prev)
+        if self.output_neurons == 1:
+            act = "sigmoid"
+        else:
+            act = "softmax"
+        out = Dense(self.output_neurons, activation=act)(prev)
         model = Model(inputs=[obj_input, event_input], outputs=out)
         lr = hp.Float("learning rate", 0.00001, 0.001, 0.000005)
         beta_1 = hp.Float("beta 1", 0.9, 0.999, 0.001)
@@ -73,7 +77,7 @@ class HyperModel(kt.HyperModel):
         return model
 
     def fit(self, hp, model, *args, **kwargs):
-        return model.fit(*args, batch_size=hp.Choice("Batch size", [16, 32, 64, 128]), **kwargs)
+        return model.fit(*args, **kwargs)
 
 
 def main(output_neurons=1):
@@ -90,56 +94,58 @@ def main(output_neurons=1):
         X_event, X_obj, y, test_size=0.15, random_state=42, shuffle=True, stratify=y
     )
     # Imbalanced dataset so want to adjusts weights of signal and background training examples
-    # Oversample hadronics to deal with massive class imbalance
     y_train, y_test = y_train.values, y_test.values  # np arrays for ease
-    hadronics_obj, hadronics_event, hadronics_labels = (
-        np.repeat(X_train_obj[y_train[:, 1] == 1], 50, axis=0),
-        np.repeat(X_train_event[y_train[:, 1] == 1], 50, axis=0),
-        np.repeat(y_train[y_train[:, 1] == 1], 50, axis=0),
-    )
-    X_train_obj, X_train_event, y_train = (
-        np.concatenate([X_train_obj, hadronics_obj]),
-        np.concatenate([X_train_event, hadronics_event]),
-        np.concatenate([y_train, hadronics_labels]),
-    )
-    indices = np.random.permutation(len(X_train_obj))
-    X_train_obj, X_train_event, y_train = (
-        X_train_obj[indices],
-        X_train_event[indices],
-        y_train[indices],
-    )
-    # weight positives more than negatives
-    sample_weights = class_weight.compute_sample_weight(
-        class_weight="balanced",
-        y=y_train,
-    )
-    test_weights = class_weight.compute_sample_weight(class_weight="balanced", y=y_test)
+    if output_neurons == 1:
+        y_train, y_test = y_train[:,-1], y_test[:,-1]
+        class_weights = class_weight.compute_class_weight(class_weight='balanced', classes=np.unique(y_train), y=y_train)
+        d_class_weights = dict(enumerate(class_weights))
+        train_weights = np.array([d_class_weights[i] for i in y_train])
+        test_weights = np.array([d_class_weights[i] for i in y_test])
+    else:
+        y_integers = np.argmax(y_train, axis=1)
+        class_weights = class_weight.compute_class_weight(class_weight='balanced', classes=np.unique(y_integers), y=y_integers)
+        d_class_weights = dict(enumerate(class_weights))
+        train_weights = np.array([d_class_weights[i] for i in np.argmax(y_train, axis=1)])
+        test_weights = np.array([d_class_weights[i] for i in np.argmax(y_test, axis=1)])
+    # Bayesian opt
     tuner = kt.BayesianOptimization(
         HyperModel(output_neurons),
-        objective=kt.Objective("val_auc", "max"),
-        max_trials=80,
+        objective='val_loss',
+        max_trials=100,
         overwrite=True,
         directory=f"kt_RNN_MLP_{output_neurons}_classes",
-        project_name="feb_24_tuning",
+        project_name="mar_1_tuning",
         beta=3,
     )
-    callbacks = [EarlyStopping(patience=10), ReduceLROnPlateau(patience=5)]
-    if output_neurons == 1:
-        y_train, y_test = y_train[:, -1], y_test[:, -1]
+    # Reduce learning rate if val loss start to increase + stop early if it doesn't decrease after 15 epochs
+    callbacks = [EarlyStopping(patience=15), ReduceLROnPlateau(patience=1)]
     tuner.search(
         [X_train_obj, X_train_event],
         y_train,
         validation_data=([X_test_obj, X_test_event], y_test, test_weights),
         callbacks=callbacks,
-        sample_weight=sample_weights,
-        epochs=30
+        sample_weight=train_weights,
+        epochs=50,
+        batch_size=64
     )
     best_HP = tuner.get_best_hyperparameters()[0]
     print(best_HP.values)
     best_model = HyperModel(output_neurons).build(best_HP)
-    best_model.save(f"./models/feb24_tuned_{output_neurons}_classes")
+    best_model.save(f"./models/mar1_tuned_{output_neurons}_classes")
 
 if __name__ == "__main__":
+    # gpu setup
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+    # Restrict TensorFlow to only use the second GPU
+        try:
+            tf.config.set_visible_devices(gpus[1], 'GPU')
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+        except RuntimeError as e:
+            # Visible devices must be set before GPUs have been initialized
+            print(e)
+
     if len(sys.argv) == 2:
         main(int(sys.argv[1]))
     else:
